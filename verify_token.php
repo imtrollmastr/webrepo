4298229e8710eee5ee10c12e59dc3361125c2c53
Eddie Stockton <130129190+imtrollmastr@users.noreply.github.com>
2023-04-22 20:40:37 +0800

    Verify xhs.OPEN tag on index.html

---
+++ verify_token.php
@@ -0,0 +1,42 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$client = new Google_Client(['client_id' => '720327052488-ibslkp5h4q5igu1ee368j939ra8op93b.apps.googleusercontent.com']);
+
+function verify_token($token) {
+  global $client;
+  try {
+    $payload = $client->verifyIdToken($token);
+    if ($payload) {
+      return $payload;
+    } else {
+      return null;
+    }
+  } catch (Exception $e) {
+    return null;
+  }
+}
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+  http_response_code(404);
+  exit();
+}
+
+$token = $_POST['idtoken'];
+$payload = verify_token($token);
+
+if ($payload) {
+  $email = $payload['email'];
+  // The token is valid, you can authenticate the user and create a session
+  // (replace this with your own authentication and session creation code)
+  session_start();
+  $_SESSION['email'] = $email;
+  echo json_encode(['success' => true]);
+} else {
+  // The token is invalid or expired, return an error response
+  http_response_code(401);
+  echo json_encode(['success' => false, 'error' => 'Token verification failed']);
+}
+
+?>
