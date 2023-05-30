684cfbb8bbba5c1fd89af606af41cb292cbfaaa1
Eddie Stockton <130129190+imtrollmastr@users.noreply.github.com>
2023-05-30 19:10:21 +0800

    restyling and return validation

--- data/validate.js
+++ data/validate.js
@@ -1,30 +1,8 @@
 function checkPassword() {
     var password = document.getElementById("passwordBox");
     var passwordText = password.value;
-    if(passwordText == "yomamaobama") {
+    if(passwordText == "password") {
       return true;
-    }
-    if(passwordText == "aquarius1984") {
-        return true;
-    }
-    if(passwordText == "braingames") {
-        return true;
-    }
-    alert("ErrorData: accessdenied; returnedVoid")
-    return false
-  }
-
-  function checkUsername() {
-    var username = document.getElementById("usernameBox");
-    var usernameText = username.value;
-    if(usernameText == "imtrollmastr") {
-      return true;
-    }
-    if(usernameText == "pooh") {
-        return true;
-    }
-    if(usernameText == "@test") {
-        return true;
     }
     alert("ErrorData: accessdenied; returnedVoid")
     return false
