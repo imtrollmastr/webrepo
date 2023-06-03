017b8cd37ffe9b2c14f12335ce403a19488f6ca9
Eddie Stockton <130129190+imtrollmastr@users.noreply.github.com>
2023-06-03 17:19:27 +0800

    restricted restart

---
+++ restricted/showdots.js
@@ -0,0 +1,6 @@
+function showText() {
+    var hiddenDots = document.getElementById("hidden-dots");
+    var hiddenText = document.getElementById("hidden-text");
+    hiddenDots.classList.add("hidden");
+    hiddenText.classList.remove("hidden");
+  }
