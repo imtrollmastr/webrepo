f5a4c154b1098bf5b6768161ebb6cd08e5a4515f
Eddie Stockton <130129190+imtrollmastr@users.noreply.github.com>
2023-06-05 14:26:34 +0800

    multiple accs

--- data/processvalidate.js
+++ data/processvalidate.js
@@ -1,8 +1,24 @@
-let password = prompt("Enter your password:");
+let username = prompt("Enter your username:");
 
+var ErrorData = "Invalid Password (ERROR403_ACCESSREQUESTED)"
+
+var passwordAlertGreeting = "Welcome, imtrollmastr!"
+
+var backupAlertGreeting = "Welcome, backupaccount5"
+
+let password = prompt("Enter your password:");
 if (password === "yomamaobama") {
-  alert("Welcome Johann!");
-  window.location.href = "openadmin.htm";
+alert(passwordAlertGreeting);
+window.location.href = "openadmin.htm";
 } else {
-  location.reload();
+alert(ErrorData)
+location.reload();
+}
+
+if (password === "YoJpingBobo123~!!(BACKUP)1[{}']") {
+alert(backupAlertGreeting)
+window.location.href = "openadmin.htm";
+} else {
+alert(ErrorData)
+location.reload()
 }
