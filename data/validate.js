9428b95458371dbb555a4be3f0fcc9eab51f6929
Eddie Stockton <130129190+imtrollmastr@users.noreply.github.com>
2023-08-11 22:11:08 -0400

    redirect

--- data/validate.js
+++ data/validate.js
@@ -1,8 +1,10 @@
+var account1 = "password"
 function checkPassword() {
     var password = document.getElementById("passwordBox");
     var passwordText = password.value;
-    if(passwordText == "password") {
+    if(passwordText == account1) {
       return true;
+      window.location.href="https://imtrollmastr.vercel.app/"
     }
     alert("ErrorData: accessdenied; returnedVoid")
     return false
